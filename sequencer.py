5e911c9d533b722d30820e0337a18b30c2be54c5
Matthew Starcher <mlstarcher@gmail.com>
2022-09-18 12:03:31 -0700

    Added function to set tempo

--- sequencer.py
+++ sequencer.py
@@ -4,7 +4,7 @@ tempo = 100
 beat_length = 60 / tempo
 
 print(beat_length)
-def drummer():
+def clock():
     counter = 0
     time.sleep(time.time() * 8 % 1 / 8) # enable to sync clock for demo
     while counter < 10:
@@ -12,4 +12,7 @@ def drummer():
         print(time.time())
         time.sleep(beat_length - time.time() * 8 % 1 / 8)
 
-drummer()
+def setTempo(bpm):
+  tempo = bpm
+
+clock()
