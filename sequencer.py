e4a53b45588b16e1303b04430016b4c9bbd730d6
Matthew Starcher <mlstarcher@gmail.com>
2022-09-18 12:01:59 -0700

    Addded tempo calculation to loop function

--- sequencer.py
+++ sequencer.py
@@ -1,11 +1,15 @@
 import time
 
+tempo = 100
+beat_length = 60 / tempo
+
+print(beat_length)
 def drummer():
     counter = 0
-    # time.sleep(time.time() * 8 % 1 / 8) # enable to sync clock for demo
-    while counter < 60 * 8:
+    time.sleep(time.time() * 8 % 1 / 8) # enable to sync clock for demo
+    while counter < 10:
         counter += 1
         print(time.time())
-        time.sleep(.125 - time.time() * 8 % 1 / 8)
+        time.sleep(beat_length - time.time() * 8 % 1 / 8)
 
 drummer()
