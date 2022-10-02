98f92271a3f2c5ff7bb2e5b56e40fb05292217eb
Matthew Starcher <mlstarcher@gmail.com>
2022-10-02 15:08:19 -0700

    Start, stop and set tempo functionality complete

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import emitter from "./emitter/index.js";
 import sequencer from "./sequencer/index.js";
+console.log("App started");
 
-emitter.on("step", () => {
-  console.log("steppin");
+emitter.once('playing', () => {
+  console.log('Sequencer starting')
+})
+
+emitter.once('stopped', () => {
+  console.log('Sequencer stopped')
+})
+
+emitter.on("step", (step) => {
+  console.log(step);
 });
 
 sequencer.play();
-console.log("App started");
+setTimeout(() => {sequencer.setTempo(120)}, 4000)
+setTimeout(() => {sequencer.setSwing(60)}, 4000)
+setTimeout(() => {sequencer.stop()}, 30000)
+
--- sequencer/msSequencer.js
+++ sequencer/msSequencer.js
@@ -7,28 +7,39 @@ class StepSequencer {
     this.sequence = sequence || [0, 0, 0, 0];
     this._playing = false;
     this.step = 0;
-    this.interval = Math.floor(60000 / (this._tempo * 2));
+    this._swing = 0;
+    //this.interval = Math.floor(60000 / (this._tempo * 2));
   }
   play() {
     if (this._playing) return;
-    console.log('Playing ran')
     this._playing = true;
+    emitter.emit('playing');
     this.clock();
+  }
+  setTempo(tempo) {
+    this._tempo = tempo;
+  }
+  setSwing(swing) {
+    this._swing = swing;
   }
   stop() {
     if (!this._playing) return;
-    this._playing = false;ß
+    this._playing = false;
+    emitter.emit('stopped')
   }
   clock() {
+    let interval = Math.floor(60000 / (this._tempo * 2));
     if (!this._playing) return;
+    // if (this.step % 2 === false) {
+    //   interval += _swing;
+    // }
     this.step++;
-    if (this.step === this.sequence.length -1) {
+    console.log('interval: ', interval)
+    if (this.step === this.sequence.length) {
       this.step = 0;
     }
-    console.log('In sequencer step: ', this.step)
-    emitter.emit('step')
-    // setTimeout(this.clock, (60 / this.tempo))
-    setTimeout(() => this.clock(), this.interval)
+    emitter.emit('step', this.step)
+    setTimeout(() => this.clock(), interval)
   }
 }
 
